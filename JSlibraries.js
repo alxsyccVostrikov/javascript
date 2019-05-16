af7dc4ad5c02983a55d203e0040244d32ec743fa
alxsyccVostrikov <48442884+alxsyccVostrikov@users.noreply.github.com>
2019-05-16 11:37:15 -0300

    Update JSlibraries.js

--- JSlibraries.js
+++ JSlibraries.js
@@ -27,6 +27,7 @@ let Alibs=args;
 for(let i=0;i<args.length;i++){
     var script = document.createElement("script");
     script.src = Alibs[i];  
- document.head.appendChild(script);
- }
+ //document.head.appendChild(script);
+ document.getElementsByTagName('head')[0].appendChild(script);
+}
 }
